2ae3d8e5945c16226ddb6e00bee4bac2046a2770
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2015-12-22 17:53:32 +0100

    Added the getKeysForSet method in the StorageManagerDecorator

---
+++ src/main/scala/sword/langbook/db/StorageManageDecorator.scala
@@ -0,0 +1,15 @@
+package sword.langbook.db
+
+class StorageManageDecorator(override val wrapped :StorageManager) extends StorageManagerWrapper {
+
+  def getKeysForSet(set: SetIdentifierFieldDefinition, id :Register.SetId) :Set[Register.Key] = {
+    val regDef = registerDefinitions.find(_.fields.contains(set)).get
+    getKeysFor(regDef).flatMap { key =>
+      get(regDef, key).filter { reg =>
+        reg.fields.collectFirst { case x: SetIdentifierField if x.definition == set && x.value == id =>
+          x
+        }.nonEmpty
+      }.map(_ => key)
+    }
+  }
+}
---
+++ src/test/scala/sword/langbook/db/StorageManagerDecoratorTest.scala
@@ -0,0 +1,33 @@
+package sword.langbook.db
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class StorageManagerDecoratorTest extends FlatSpec with Matchers {
+
+  def newStorageManagerDecorator(definitions :Seq[RegisterDefinition]) =
+      new StorageManageDecorator(new MemoryStorageManager(definitions))
+
+  object setFieldDef extends SetIdentifierFieldDefinition
+
+  val regDefWithSet = new RegisterDefinition {
+    override val fields = List(setFieldDef)
+  }
+
+  behavior of "The Storage Manager Decorator"
+
+  it should "return all register keys for those register with the given set id" in {
+    val manager = newStorageManagerDecorator(List(regDefWithSet))
+    val ids = List[Register.SetId](1,2,3,1,2,1)
+    val keys = for (id <- ids) yield {
+      manager.insert(new Register {
+        override val fields: Seq[Field] = List(SetIdentifierField(setFieldDef, id))
+        override val definition = regDefWithSet
+      }).get
+    }
+
+    for (id <- ids.min until ids.max) {
+      val expected = ids zip keys filter { case (i,_) => i == id } map { x => x._2 }
+      manager.getKeysForSet(setFieldDef, id) shouldBe expected.toSet
+    }
+  }
+}

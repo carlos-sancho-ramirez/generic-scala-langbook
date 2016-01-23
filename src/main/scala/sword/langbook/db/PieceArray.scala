a9ffc64d9b400299d2482cb9222f4c036d19895a
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-01-23 15:21:50 +0100

    Implemented PieceArray within the linked layer

---
+++ src/main/scala/sword/langbook/db/PieceArray.scala
@@ -0,0 +1,21 @@
+package sword.langbook.db
+
+import sword.db.{CollectionReferenceField, Register, StorageManager}
+
+case class PieceArray(storageManager :StorageManager, arrayId :Register.CollectionId) extends Seq[Piece] {
+
+  def pieceKeys = storageManager.getKeysForArray(registers.PiecePosition, arrayId)
+  def fields(key :StorageManager.Key) = key.registerOption.map(_.fields).getOrElse(Seq())
+  def pieceCollIdOpt(key :StorageManager.Key) = fields(key).collectFirst {
+    case field :CollectionReferenceField if field.definition.target == registers.Piece => field.collectionId
+  }
+
+  override def length = pieceKeys.length
+  override def apply(idx :Int) = Piece(storageManager, pieceCollIdOpt(pieceKeys(idx)).get)
+
+  override def iterator = new Iterator[Piece] {
+    val keys = pieceKeys.iterator
+    override def hasNext = keys.hasNext
+    override def next() = Piece(storageManager, pieceCollIdOpt(keys.next()).get)
+  }
+}

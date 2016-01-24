c9ac853f5b817ff730d59d9f9e9b2595a2d1293f
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-01-24 15:06:04 +0100

    Fix on the Symbol key retireval and added some guard code

--- src/main/scala/sword/langbook/db/Symbol.scala
+++ src/main/scala/sword/langbook/db/Symbol.scala
@@ -3,6 +3,10 @@ package sword.langbook.db
 import sword.db.{UnicodeField, StorageManager}
 
 case class Symbol(key :StorageManager.Key) {
+  if (key.registerDefinition != registers.Symbol) {
+    throw new IllegalArgumentException("Wrong registerDefinition within the key for a Symbol")
+  }
+
   def fields = key.registerOption.map(_.fields).getOrElse(Seq())
   def unicodeOpt = fields.collectFirst {
     case field :UnicodeField => field.value
--- src/main/scala/sword/langbook/db/SymbolArray.scala
+++ src/main/scala/sword/langbook/db/SymbolArray.scala
@@ -1,16 +1,24 @@
 package sword.langbook.db
 
-import sword.db.{Register, StorageManager}
+import sword.db.{ForeignKeyField, Register, StorageManager}
 
 case class SymbolArray(storageManager :StorageManager, arrayId :Register.CollectionId) extends Seq[Symbol] {
 
   def symbolKeys = storageManager.getKeysForArray(registers.SymbolPosition, arrayId)
+  if (symbolKeys.isEmpty) {
+    throw new IllegalArgumentException("Wrong arrayId for a SymbolArray")
+  }
+
+  def targetSymbolKey(key :StorageManager.Key) = key.registerOption.flatMap(_.fields.collectFirst {
+    case field :ForeignKeyField if field.definition.target == registers.Symbol => field.key
+  })
+
   override def length = symbolKeys.length
   override def apply(idx :Int) = Symbol(symbolKeys(idx))
 
   override def iterator = new Iterator[Symbol] {
     val symbols = symbolKeys.iterator
     override def hasNext = symbols.hasNext
-    override def next() = Symbol(symbols.next())
+    override def next() = Symbol(targetSymbolKey(symbols.next()).get)
   }
 }

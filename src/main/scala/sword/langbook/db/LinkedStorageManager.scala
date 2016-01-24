8df78908237fe24665631960e5595fcec65520d9
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-01-24 14:06:38 +0100

    Added the rest of regsterDefinition to the linked layer

--- src/main/scala/sword/langbook/db/LinkedStorageManager.scala
+++ src/main/scala/sword/langbook/db/LinkedStorageManager.scala
@@ -3,7 +3,18 @@ package sword.langbook.db
 import sword.db.{RegisterDefinition, StorageManager}
 
 case class LinkedStorageManager(storageManagerFactory :(List[RegisterDefinition]) => StorageManager) {
-  val registerDefinitions = List(registers.Concept, registers.Alphabet, registers.Language)
+  val registerDefinitions = List(
+      registers.Concept,
+      registers.Alphabet,
+      registers.Language,
+      registers.LanguageAlphabet,
+      registers.Piece,
+      registers.PiecePosition,
+      registers.Symbol,
+      registers.SymbolPosition,
+      registers.Word,
+      registers.WordConcept)
+
   val storageManager = storageManagerFactory(registerDefinitions)
 
   def concepts = storageManager.getMapFor(registers.Concept).collect { case (k, v :registers.Concept) => (k,v)}
@@ -12,5 +23,9 @@ case class LinkedStorageManager(storageManagerFactory :(List[RegisterDefinition]
   }
   def languages = storageManager.getKeysFor(registers.Language).groupBy(x => x).map { case (key, _) =>
     (key, Language(key))
+  }
+
+  def words = storageManager.getKeysFor(registers.Word).groupBy(x => x).map { case (key, _) =>
+    (key, Word(key))
   }
 }

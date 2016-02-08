07839cb5b096eb2196a2373487410ebff51f0978
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-02-08 22:12:08 +0100

    Allow retrieving a word in its string representation

--- src/main/scala/sword/langbook/db/Word.scala
+++ src/main/scala/sword/langbook/db/Word.scala
@@ -13,4 +13,14 @@ case class Word(key :StorageManager.Key) {
 
   def language = Language(languageKeyOpt.get)
   def pieces = PieceArray(key.storageManager, pieceArrayIdOpt.get)
+
+  /**
+    * Returns in a string the representation of this word in the given alphabet if any.
+    * @return A Some instance containing the string or None if there is no representation for the
+    *         given alphabet.
+    */
+  def text(alphabet: Alphabet): Option[String] = {
+    // TODO: To be improved to return None instead of Some("") for wrong alphabets
+    Some(pieces.flatMap(_.get(alphabet)).flatMap(x => x).map(_.unicode.toChar).mkString(""))
+  }
 }

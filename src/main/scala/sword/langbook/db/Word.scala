bc639d4c1842510e9d884e55da3c9a9a1ffef6ac
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-02-12 15:00:42 +0100

    Change Word.text to be a map

--- src/main/scala/sword/langbook/db/Word.scala
+++ src/main/scala/sword/langbook/db/Word.scala
@@ -15,14 +15,35 @@ case class Word(key :StorageManager.Key) {
   def pieces = PieceArray(key.storageManager, pieceArrayIdOpt.get)
 
   /**
-    * Returns in a string the representation of this word in the given alphabet if any.
-    *
-    * @return A Some instance containing the string or None if there is no representation for the
-    *         given alphabet.
+    * Returns a map match alphabets and its string representations
     */
-  def text(alphabet: Alphabet): Option[String] = {
-    // TODO: To be improved to return None instead of Some("") for wrong alphabets
-    Some(pieces.flatMap(_.get(alphabet)).flatMap(x => x).map(_.unicode.toChar).mkString(""))
+  def text: Map[Alphabet, String] = new Map[Alphabet, String] {
+    private def trustyGet(alphabet :Alphabet) = {
+      pieces.flatMap(_.get(alphabet)).flatMap(x => x).map(_.unicode.toChar).mkString("")
+    }
+
+    override def get(alphabet: Alphabet): Option[String] = {
+      if (pieces.forall(_.contains(alphabet))) {
+        Some(trustyGet(alphabet))
+      }
+      else None
+    }
+
+    override def iterator: Iterator[(Alphabet, String)] = new Iterator[(Alphabet, String)] {
+      private val it = pieces.foldLeft(Set[Alphabet]())((set, piece) => set ++ piece.keys).filter {
+        alphabet => pieces.forall(_.contains(alphabet))
+      }.iterator
+
+      override def hasNext = it.hasNext
+      override def next(): (Alphabet, String) = {
+        val alphabet = it.next()
+        (alphabet, trustyGet(alphabet))
+      }
+    }
+
+    // TODO: Implement these methods
+    override def +[B1 >: String](kv: (Alphabet, B1)): Map[Alphabet, B1] = ???
+    override def -(key: Alphabet): Map[Alphabet, String] = ???
   }
 
   /**

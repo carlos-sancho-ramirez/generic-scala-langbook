06d2c6aed9d56dcc714308743b18141e1fc1a013
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-03-17 17:44:22 +0100

    Add test to insert concept

--- src/test/scala/sword/langbook/db/LinkedStorageManagerTest.scala
+++ src/test/scala/sword/langbook/db/LinkedStorageManagerTest.scala
@@ -10,4 +10,16 @@ class LinkedStorageManagerTest extends FlatSpec with Matchers {
   it can "be created" in {
     LinkedStorageManager(defs => new MemoryStorageManager(defs))
   }
+
+  it can "insert a concept" in {
+    val manager = LinkedStorageManager(defs => new MemoryStorageManager(defs))
+    manager.concepts shouldBe empty
+
+    val conceptOption = manager.addConcept(ConceptParams("MyConcept"))
+    conceptOption shouldBe defined
+
+    val concepts = manager.concepts
+    concepts.size shouldBe 1
+    concepts.values.head shouldBe conceptOption.get
+  }
 }

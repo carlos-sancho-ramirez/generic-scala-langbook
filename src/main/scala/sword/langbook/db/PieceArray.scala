b32d6d6ac807dcccdd833b9b3404175f609bce64
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-04-16 16:00:32 +0200

    Add code to retrieve text from PieceArrays

--- src/main/scala/sword/langbook/db/PieceArray.scala
+++ src/main/scala/sword/langbook/db/PieceArray.scala
@@ -2,6 +2,8 @@ package sword.langbook.db
 
 import sword.db.{CollectionReferenceField, Register, StorageManager}
 
+import scala.collection.AbstractIterator
+
 case class PieceArray(storageManager :StorageManager, arrayId :Register.CollectionId) extends Seq[Piece] {
 
   def pieceKeys = storageManager.getKeysForArray(registers.PiecePosition, arrayId)
@@ -17,6 +19,44 @@ case class PieceArray(storageManager :StorageManager, arrayId :Register.Collecti
     val keys = pieceKeys.iterator
     override def hasNext = keys.hasNext
     override def next() = Piece(storageManager, pieceCollIdOpt(keys.next()).get)
+  }
+
+  /**
+    * Returns a map match alphabets and its string representations
+    */
+  def text: Map[Alphabet, String] = new Map[Alphabet, String] {
+
+    /**
+     * So far, valid alphabet are those that all pieces includes
+     */
+    private def validAlphabets = PieceArray.this.map(_.text.keySet).reduce((a,b) => a.intersect(b))
+
+    private def trustyGet(alphabet :Alphabet) = {
+      PieceArray.this.map(_.text(alphabet)).mkString
+    }
+
+    override def get(alphabet: Alphabet): Option[String] = {
+      PieceArray.this.foldLeft(Option(""))((strOpt, piece) =>
+          strOpt.flatMap(str => piece.text.get(alphabet).map(str + _)))
+    }
+
+    override def iterator = new AbstractIterator[(Alphabet, String)] {
+      private val it = validAlphabets.iterator
+
+      override def hasNext = it.hasNext
+      override def next() = {
+        val alphabet = it.next()
+
+        // Here we are assuming that the value will be there while still iterating.
+        // trustyGet could crash if something changed in the meanwhile
+        // TODO: Avoid any crash
+        (alphabet, trustyGet(alphabet))
+      }
+    }
+
+    // TODO: Implement these methods
+    override def +[B1 >: String](kv: (Alphabet, B1)): Map[Alphabet, B1] = ???
+    override def -(key: Alphabet): Map[Alphabet, String] = ???
   }
 }
 
--- src/main/scala/sword/langbook/db/Word.scala
+++ src/main/scala/sword/langbook/db/Word.scala
@@ -6,48 +6,11 @@ import sword.langbook.db.registers.WordReferenceField
 import scala.collection
 
 case class Word(key :StorageManager.Key) {
-  def fields = key.registerOption.map(_.fields).getOrElse(Seq())
-  def languageKeyOpt = fields.collectFirst {
-    case field :ForeignKeyField if field.definition.target == registers.Language => field.key
-  }
-  def pieceArrayIdOpt = fields.collectFirst {
-    case field :CollectionReferenceField if field.definition.target == registers.PiecePosition => field.collectionId
-  }
+  private def wordReg = key.registerOption.get.asInstanceOf[registers.Word]
+  def language = Language(wordReg.language)
+  def pieces = PieceArray(key.storageManager, wordReg.pieceArray)
 
-  def language = Language(languageKeyOpt.get)
-  def pieces = PieceArray(key.storageManager, pieceArrayIdOpt.get)
-
-  /**
-    * Returns a map match alphabets and its string representations
-    */
-  def text: Map[Alphabet, String] = new Map[Alphabet, String] {
-    private def trustyGet(alphabet :Alphabet) = {
-      pieces.flatMap(_.get(alphabet)).flatMap(x => x).map(_.unicode.toChar).mkString("")
-    }
-
-    override def get(alphabet: Alphabet): Option[String] = {
-      if (pieces.forall(_.contains(alphabet))) {
-        Some(trustyGet(alphabet))
-      }
-      else None
-    }
-
-    override def iterator: Iterator[(Alphabet, String)] = new Iterator[(Alphabet, String)] {
-      private val it = pieces.foldLeft(Set[Alphabet]())((set, piece) => set ++ piece.keys).filter {
-        alphabet => pieces.forall(_.contains(alphabet))
-      }.iterator
-
-      override def hasNext = it.hasNext
-      override def next(): (Alphabet, String) = {
-        val alphabet = it.next()
-        (alphabet, trustyGet(alphabet))
-      }
-    }
-
-    // TODO: Implement these methods
-    override def +[B1 >: String](kv: (Alphabet, B1)): Map[Alphabet, B1] = ???
-    override def -(key: Alphabet): Map[Alphabet, String] = ???
-  }
+  def text = pieces.text
 
   /**
    * Return the suitable human readable string for this word based on the preferredAlphabet
